4ee5eebaeb143727fbc25959e7ff9d28f8f0fe3d
hayatasue <69018585+hayatasue@users.noreply.github.com>
2020-11-02 14:46:13 -0500

    Added D3

---
+++ card.js
@@ -0,0 +1,73 @@
+var employeePromise = d3.json("employee.json");
+
+var getPic = function(employee){
+    return employee.photo;
+}
+
+var getName = function(employee){
+    return employee.firstName + employee.lastName;
+}
+
+var getTitle = function(employee){
+    return employee.title;
+}
+
+var getUnit = function(employee){
+    return employee.unit;
+}
+
+var getEmail = function(employee){
+    return employee.email;
+}
+
+var getBio = function(employee){
+    return employee.bio;
+}
+
+var successFun = function(empList)
+{
+    var employee = empList.slice(0,2);
+    
+    var employeeDiv = d3.select("body")
+        .selectAll("div")
+        .data(employee)
+        .enter()
+        .append("div")
+        .classed("employee", true)
+    
+    var generalDiv = employeeDiv.append("div")
+            .classed("general", true);
+        
+    generalDiv.append("span")
+        .classed("eName")
+        .text(getName);
+    
+    generalDiv.append("img")
+        .attr("src", getPic);
+    
+    var detailDiv = employeeDiv.append("div")
+            .classed("detail", true);
+    
+    detailDiv.append("span")
+        .classed("title", true)
+        .text(getTitle);
+    
+    detailDiv.append("span")
+        .classed("department", true)
+        .text(getUnit);
+    
+    detailDiv.append("span")
+        .classed("email", true)
+        .text(getEmail);
+    
+    detailDiv.append("p")
+        .classed("bio", true)
+        .text(getBio);
+}
+
+var failFun = function(error)
+{
+    console.log("Error:", error);
+}
+
+employeePromise.then(successFun, failFun)
---
+++ index.js
@@ -0,0 +1,76 @@
+var employeePromise = d3.json("employee.json");
+
+var getPic = function(employee){
+    return employee.photo;
+}
+
+var getFirstName = function(employee){
+    return employee.firstName;
+}
+
+var getLastName = function(employee){
+    return employee.lastName;
+}
+
+var getTitle = function(employee){
+    return employee.title;
+}
+
+var getUnit = function(employee){
+    return employee.unit;
+}
+
+var getPhone = function(employee){
+    return employee.phone;
+}
+
+var getEmail = function(employee){
+    return employee.email;
+}
+
+var getBio = function(employee){
+    return employee.bio;
+}
+
+var successFun = function(empList)
+{
+    var employee = empList.slice(0,2);
+    
+    var rows = d3.select("tbody")
+        .selectAll("tr")
+        .data(employee)
+        .enter()
+        .append("tr")
+    
+    rows.append("td")
+        .append("img")
+        .attr("src", getPic);
+    
+    rows.append("td")
+        .text(getFirstName);
+    
+    rows.append("td")
+        .text(getLastName);
+    
+    rows.append("td")
+        .text(getTitle);
+    
+    rows.append("td")
+        .text(getUnit);
+    
+    rows.append("td")
+        .text(getPhone);
+    
+    rows.append("td")
+        .text(getEmail);
+    
+    rows.append("td")
+        .text(getBio);
+}
+
+var failFun = function(error)
+{
+    console.log("Error:", error);
+}
+
+employeePromise.then(successFun, failFun)
